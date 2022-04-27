2b761da1ca2a25c7ca9e436a63e7c181a4031477
KingMob4313 <shortstop4313@gmail.com>
2022-04-27 18:57:17 -0400

    Added config and some parsers

---
+++ MasterCrewExplorer/MasterClassController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MasterCrewExplorer
+{
+    class MasterClassController
+    {
+        object GetMasterCrewData(string url)
+        {
+            string jsonData = GetJsonData(url);
+
+            return new object();
+        }
+
+        private static string GetJsonData(string url)
+        {
+            using (WebClient currentWebClient = new WebClient())
+            {
+                string json = currentWebClient.DownloadString(url);
+                return json;
+            }
+        }
+    }
+}
---
+++ MasterCrewExplorer/MasterCrewParser.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MasterCrewExplorer
+{
+    class MasterCrewParser
+    {
+        //Need to divide the json objects into individual lists of objects
+
+    }
+}
