f143e5c75a4631ec2c7cf204352b41d42feb1660
Frederick Bell <shortstop4313@gmail.com>
2022-04-30 16:08:53 -0400

    Missing files

---
+++ MasterCrewExplorer/MartialArtist.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MasterCrewExplorer
+{
+    class MartialArtist
+    {
+        private int hitPoints;
+
+        public int HitPoints { get => hitPoints; set => hitPoints = value; }
+    }
+}
