fc8c91da7356be41402cc01e327814445314a106
Frederick Bell <shortstop4313@gmail.com>
2022-05-18 14:09:34 -0400

    Using Newtonsoft json parser, last checkin before changing to something else

--- MasterCrewExplorer/MasterCrewParser.cs
+++ MasterCrewExplorer/MasterCrewParser.cs
@@ -16,8 +16,25 @@ namespace MasterCrewExplorer
 
         public DnD5thCharacter ParseJsonToDataList(string jsonString, DnD5thCharacter currentDnDCharacter)
         {
-            //var xyz = JsonConvert.DeserializeObject(jsonString, typeof));
-            var abc = JsonConvert.DeserializeObject<List<DnD5thCharacter>>(jsonString);
+            //var xyz = JsonConvert.DeserializeObject(jsonString);
+            JArray xyz = JArray.Parse(jsonString);
+            List<DnD5thCharacter> characterList = new List<DnD5thCharacter>();
+            //ChildrenTokens xxx = null;
+            foreach (JToken yyy in xyz)
+            {
+                DnD5thCharacter currentCharacter = new DnD5thCharacter();
+                JEnumerable<JToken> tempChar = yyy.Children();
+                var asadsd = tempChar.Values("Strength");
+                if (tempChar[2].ToString() != null)
+                {
+                    int tempStr = 0;
+                    int.TryParse(tempChar["Strength"].ToString(), out tempStr);
+                    currentCharacter.Strength = tempStr;
+                }
+
+            }
+
+            //var abc = JsonConvert.DeserializeObject<List<DnD5thCharacter>>(jsonString);
             return currentDnDCharacter;
         }
     }
