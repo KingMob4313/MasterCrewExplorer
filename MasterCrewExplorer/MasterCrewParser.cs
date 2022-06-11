56a62c4309526833dfd2555dcdc2c4a5d34d39b2
Frederick Bell <shortstop4313@gmail.com>
2022-06-10 23:47:29 -0400

    LOL wut a mess

--- MasterCrewExplorer/MasterCrewParser.cs
+++ MasterCrewExplorer/MasterCrewParser.cs
@@ -1,8 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using Newtonsoft.Json;
+﻿using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
-
+using System;
+using System.Collections.Generic;
 
 namespace MasterCrewExplorer
 {
@@ -17,22 +16,59 @@ namespace MasterCrewExplorer
         public DnD5thCharacter ParseJsonToDataList(string jsonString, DnD5thCharacter currentDnDCharacter)
         {
             //var xyz = JsonConvert.DeserializeObject(jsonString);
+
             JArray xyz = JArray.Parse(jsonString);
             List<DnD5thCharacter> characterList = new List<DnD5thCharacter>();
-            //ChildrenTokens xxx = null;
-            foreach (JToken yyy in xyz)
+            List<JObject> CurrentEntry = JsonConvert.DeserializeObject<JArray>(jsonString).ToObject<List<JObject>>();
+
+            foreach (JObject dndPerson in CurrentEntry)
             {
                 DnD5thCharacter currentCharacter = new DnD5thCharacter();
-                JEnumerable<JToken> tempChar = yyy.Children();
-                var asadsd = tempChar.Values("Strength");
-                if (tempChar[2].ToString() != null)
+                var taco = dndPerson.SelectToken("Strength");
+                if (taco == null)
                 {
-                    int tempStr = 0;
-                    int.TryParse(tempChar["Strength"].ToString(), out tempStr);
-                    currentCharacter.Strength = tempStr;
+                    continue;
+                }
+                if (taco.HasValues == false)
+                {
+                    continue;
                 }
 
+                JToken strengthToken = null;
+                JToken dexterityToken = null;
+                JToken constitutionToken = null;
+                JToken intelligenceToken = null;
+                JToken wisdomToken = null;
+                JToken charismaToken = null;
+                dndPerson.TryGetValue("Strength", out strengthToken);
+                if (strengthToken != null) continue;
+                dndPerson.TryGetValue("Dexterity", out dexterityToken);
+                dndPerson.TryGetValue("Constitution", out constitutionToken);
+                dndPerson.TryGetValue("Intelligence", out intelligenceToken);
+                dndPerson.TryGetValue("Wisdom", out wisdomToken);
+                dndPerson.TryGetValue("Charisma", out charismaToken);
             }
+
+            //foreach (JToken yyy in xyz)
+            //{
+            //    DnD5thCharacter currentCharacter = new DnD5thCharacter();
+            //    var lodogsd = yyy.Children()["Strength"].Value<string>();
+            //    foreach (JToken zzz in yyy)
+            //    {
+            //        string vel = zzz.First.Value<string>();
+
+
+            //        if(vel == "1")
+            //        {
+            //            break;
+            //        }
+            //        else
+            //        {
+            //            string stre = zzz.SelectToken("$.Strength").Value<string>();
+            //        }
+
+            //    }
+            //}
 
             //var abc = JsonConvert.DeserializeObject<List<DnD5thCharacter>>(jsonString);
             return currentDnDCharacter;
