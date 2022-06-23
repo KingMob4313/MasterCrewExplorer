bbb40318a3ea1007b5d26fba3401d44ca2e62dce
Frederick Bell <shortstop4313@gmail.com>
2022-06-22 23:24:17 -0400

    Used the base web json de-serialize tools and it works now WTF

--- MasterCrewExplorer/DnDBasicsCharacter.cs
+++ MasterCrewExplorer/DnDBasicsCharacter.cs
@@ -2,30 +2,30 @@
 {
     abstract class DnDBasicsCharacter : Adventurer
     {
-        private int strength;
-        private int dexterity;
-        private int constitution;
-        private int intelligence;
-        private int wisdom;
-        private int charisma;
-        private int speed;
+        private int? strength;
+        private int? dexterity;
+        private int? constitution;
+        private int? intelligence;
+        private int? wisdom;
+        private int? charisma;
+        private int? speed;
 
-        private int level;
-        private int pcClass;
-        private int armorClass;
+        private int? level;
+        private int? pcClass;
+        private int? armorClass;
         private int[,] alignment;
 
-        public int Strength { get => strength; set => strength = value; }
-        public int Dexterity { get => dexterity; set => dexterity = value; }
-        public int Constitution { get => constitution; set => constitution = value; }
-        public int Intelligence { get => intelligence; set => intelligence = value; }
-        public int Wisdom { get => wisdom; set => wisdom = value; }
-        public int Charisma { get => charisma; set => charisma = value; }
-        public int Level { get => level; set => level = value; }
-        public int PCClass { get => pcClass; set => pcClass = value; }
+        public int? Strength { get => strength; set => strength = value; }
+        public int? Dexterity { get => dexterity; set => dexterity = value; }
+        public int? Constitution { get => constitution; set => constitution = value; }
+        public int? Intelligence { get => intelligence; set => intelligence = value; }
+        public int? Wisdom { get => wisdom; set => wisdom = value; }
+        public int? Charisma { get => charisma; set => charisma = value; }
+        public int? Level { get => level; set => level = value; }
+        public int? PCClass { get => pcClass; set => pcClass = value; }
         //Working to lower armor classes
-        public int ArmorClass { get => (20 - armorClass); }
-        public int Speed { get => speed; set => speed = value; }
+        public int? ArmorClass { get => (20 - armorClass); }
+        public int? Speed { get => speed; set => speed = value; }
         public int[,] Alignment { get => alignment; set => alignment = value; }
     }
 }
--- MasterCrewExplorer/MasterCrewParser.cs
+++ MasterCrewExplorer/MasterCrewParser.cs
@@ -1,5 +1,4 @@
-﻿using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
+﻿using System.Web.Script.Serialization;
 using System;
 using System.Collections.Generic;
 
@@ -15,62 +14,13 @@ namespace MasterCrewExplorer
 
         public DnD5thCharacter ParseJsonToDataList(string jsonString, DnD5thCharacter currentDnDCharacter)
         {
+            DnD35Character currentC = new DnD35Character();
             //var xyz = JsonConvert.DeserializeObject(jsonString);
+            var jss = new JavaScriptSerializer();
+            List<DnD5thCharacter> results = jss.Deserialize<List<DnD5thCharacter>>(jsonString);
 
-            JArray xyz = JArray.Parse(jsonString);
-            List<DnD5thCharacter> characterList = new List<DnD5thCharacter>();
-            List<JObject> CurrentEntry = JsonConvert.DeserializeObject<JArray>(jsonString).ToObject<List<JObject>>();
+            //Jesus Christ
 
-            foreach (JObject dndPerson in CurrentEntry)
-            {
-                DnD5thCharacter currentCharacter = new DnD5thCharacter();
-                var taco = dndPerson.SelectToken("Strength");
-                if (taco == null)
-                {
-                    continue;
-                }
-                if (taco.HasValues == false)
-                {
-                    continue;
-                }
-
-                JToken strengthToken = null;
-                JToken dexterityToken = null;
-                JToken constitutionToken = null;
-                JToken intelligenceToken = null;
-                JToken wisdomToken = null;
-                JToken charismaToken = null;
-                dndPerson.TryGetValue("Strength", out strengthToken);
-                if (strengthToken != null) continue;
-                dndPerson.TryGetValue("Dexterity", out dexterityToken);
-                dndPerson.TryGetValue("Constitution", out constitutionToken);
-                dndPerson.TryGetValue("Intelligence", out intelligenceToken);
-                dndPerson.TryGetValue("Wisdom", out wisdomToken);
-                dndPerson.TryGetValue("Charisma", out charismaToken);
-            }
-
-            //foreach (JToken yyy in xyz)
-            //{
-            //    DnD5thCharacter currentCharacter = new DnD5thCharacter();
-            //    var lodogsd = yyy.Children()["Strength"].Value<string>();
-            //    foreach (JToken zzz in yyy)
-            //    {
-            //        string vel = zzz.First.Value<string>();
-
-
-            //        if(vel == "1")
-            //        {
-            //            break;
-            //        }
-            //        else
-            //        {
-            //            string stre = zzz.SelectToken("$.Strength").Value<string>();
-            //        }
-
-            //    }
-            //}
-
-            //var abc = JsonConvert.DeserializeObject<List<DnD5thCharacter>>(jsonString);
             return currentDnDCharacter;
         }
     }
